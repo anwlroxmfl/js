2047e82546970f92c05a129c886b7a0f270ea1eb
anwlroxmfl <dongeun9895@gmail.com>
2026-09-15 10:49:00 +0900

    DOM

---
+++ 0915.js
@@ -0,0 +1,11 @@
+// DOM 구조 - html 구조 => 활용해서 js 로 써먹는
+// 웹 요소에 접근하기
+
+document.querySelector(선택자) - 하나만 선택할 때 => 한개의 값만 반환
+document.querySelectorAll(선택자) - 두 개 이상 선택할 때 => 노드 리스트의 형태로 변환
+
+
+document.getElementById() // id를 통해 접근
+document.getElementByClassName() // class를 통해 접근
+document.getElementByTagName() // tag를 통해 접근
+
